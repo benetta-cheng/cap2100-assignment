d74542cd79b93490f3bff162c42d10c703c62e24
Jose4fun <chengbenetta@gmail.com>
2020-11-14 00:50:36 +0800

    Update session_id to be nullable in leave_action table

--- database/migrations/2020_11_08_065918_update_foreign_keys_in_leave_action.php
+++ database/migrations/2020_11_08_065918_update_foreign_keys_in_leave_action.php
@@ -14,7 +14,7 @@ class UpdateForeignKeysInLeaveAction extends Migration
     public function up()
     {
         Schema::table('leave_action', function (Blueprint $table) {
-            $table->string('session_id');
+            $table->string('session_id')->nullable();
             $table->foreign('session_id')->references('session_id')->on('session');
             $table->dropForeign('leave_action_staff_id_foreign');
             $table->dropForeign('leave_action_course_id_foreign');
@@ -36,7 +36,7 @@ class UpdateForeignKeysInLeaveAction extends Migration
             $table->foreign('staff_id')->references('staff_id')->on('staff');
             $table->foreign('course_id')->references('course_id')->on('course');
             $table->dropForeign('leave_action_session_id_foreign');
-            $table->dropColumn('session_id');
+            $table->dropColumn('session_id')->nullable();
         });
     }
 }
